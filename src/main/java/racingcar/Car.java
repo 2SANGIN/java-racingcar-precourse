5b221d2e12b41d42750e6ccaaf73a6912baad63d
silee <silee9019@gmail.com>
2021-05-04 21:30:53 +0900

    feat: 자동차 출력 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Car {
+	private String name;
 	private int mileage = 0;
+
+	public Car(String name) {
+		this.name = name;
+	}
 
 	public void start(final int fuel) {
 		if (fuel >= 4) {
@@ -11,5 +18,10 @@ public class Car {
 
 	public int getMileage() {
 		return this.mileage;
+	}
+
+	@Override
+	public String toString() {
+		return this.name + " : " + StringUtils.repeat('-', this.mileage);
 	}
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -12,7 +12,7 @@ public class CarTest {
 	@DisplayName("자동차 멈춰")
 	@ValueSource(ints = {0, 1, 2, 3})
 	void testNotMove(int fuel) {
-		Car car = new Car();
+		Car car = new Car("test");
 		car.start(fuel);
 		assertThat(car.getMileage()).isEqualTo(0);
 	}
@@ -21,7 +21,7 @@ public class CarTest {
 	@DisplayName("자동차 전진")
 	@ValueSource(ints = {4, 5, 6, 7, 8, 9})
 	void testMove(int fuel) {
-		Car car = new Car();
+		Car car = new Car("test");
 		car.start(fuel);
 		assertThat(car.getMileage()).isEqualTo(1);
 	}
@@ -29,7 +29,7 @@ public class CarTest {
 	@Test
 	void testMoves() {
 		int[] fuels = {1, 0, 5, 2, 9, 4, 0};
-		Car car = new Car();
+		Car car = new Car("test");
 		for (int fuel : fuels) {
 			car.start(fuel);
 		}
