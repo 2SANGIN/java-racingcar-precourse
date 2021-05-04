e3e915665ee96cdc123c8bce1343681b8da4d167
silee <silee9019@gmail.com>
2021-05-04 22:00:07 +0900

    feat: 우승자 찾기

---
+++ src/main/java/racingcar/CarRacingHost.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CarRacingHost {
+	public static List<Car> award(List<Car> cars) {
+		Map<Integer, List<Car>> rankings = cars.stream()
+			.collect(Collectors.groupingBy(Car::getMileage));
+		int maxMileage = rankings.keySet()
+			.stream()
+			.max(Comparator.naturalOrder())
+			.orElseThrow(() -> new IllegalArgumentException("우승자가 없습니다."));
+		return rankings.get(maxMileage);
+	}
+}
---
+++ src/test/java/racingcar/CarRacingHostTest.java
@@ -0,0 +1,62 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+class CarRacingHostTest {
+	@Test
+	void testCarRacing() {
+		Car kia = new Car("kia");
+		kia.start(5);
+		kia.start(3);
+		kia.start(9);
+		Car bmw = new Car("bmw");
+		bmw.start(1);
+		bmw.start(2);
+		bmw.start(3);
+		Car audi = new Car("audi");
+		audi.start(5);
+		audi.start(5);
+		audi.start(5);
+
+		assertThat(CarRacingHost.award(Arrays.asList(kia, bmw, audi))).isEqualTo(Collections.singletonList(audi));
+	}
+
+	@Test
+	void testTripleWinner() {
+		Car k3 = new Car("k3");
+		k3.start(5);
+		k3.start(3);
+		k3.start(9);
+		Car k5 = new Car("k5");
+		k5.start(3);
+		k5.start(5);
+		k5.start(9);
+		Car k8 = new Car("k8");
+		k8.start(9);
+		k8.start(3);
+		k8.start(5);
+
+		assertThat(CarRacingHost.award(Arrays.asList(k3, k5, k8))).isEqualTo(Arrays.asList(k3, k5, k8));
+	}
+
+	@Test
+	void testSoloRacing() {
+		Car kia = new Car("kia");
+		kia.start(5);
+		kia.start(3);
+		kia.start(9);
+
+		assertThat(CarRacingHost.award(Collections.singletonList(kia))).isEqualTo(Collections.singletonList(kia));
+	}
+
+	@Test
+	void testNoCars() {
+		assertThatIllegalArgumentException().isThrownBy(() -> CarRacingHost.award(Collections.emptyList()))
+			.withMessage("우승자가 없습니다.");
+	}
+}
