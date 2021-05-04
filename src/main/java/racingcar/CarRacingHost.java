a8ba57bd3d53ffb4551df0ad9855aef9937e7a64
silee <silee9019@gmail.com>
2021-05-04 23:07:16 +0900

    refactor: method 분리

--- src/main/java/racingcar/CarRacingHost.java
+++ src/main/java/racingcar/CarRacingHost.java
@@ -7,12 +7,19 @@ import java.util.stream.Collectors;
 
 public class CarRacingHost {
 	public static List<Car> getWinners(List<Car> cars) {
-		Map<Integer, List<Car>> rankings = cars.stream()
-			.collect(Collectors.groupingBy(Car::getMileage));
-		int maxMileage = rankings.keySet()
+		Map<Integer, List<Car>> records = getRecords(cars);
+		int maxMileage = findMaxMileage(records);
+		return records.get(maxMileage);
+	}
+
+	private static Map<Integer, List<Car>> getRecords(List<Car> cars) {
+		return cars.stream().collect(Collectors.groupingBy(Car::getMileage));
+	}
+
+	private static int findMaxMileage(Map<Integer, List<Car>> records) {
+		return records.keySet()
 			.stream()
 			.max(Comparator.naturalOrder())
 			.orElseThrow(() -> new IllegalArgumentException("우승자가 없습니다."));
-		return rankings.get(maxMileage);
 	}
 }
